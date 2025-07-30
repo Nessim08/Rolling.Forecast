18a827de70dc21ec5d85ed2f5a915c9edeadfa52
Nessim08 <nessim.esquenazi@pernod-ricard.com>
2025-07-30 12:06:14 -0500

    Update transform.py

--- src/transform.py
+++ src/transform.py
@@ -1,1 +1,11 @@
+def aggregate(df):
+    return (
+      df
+      .groupby(['Market','Channel','SKU','Date'])
+      .agg({'Sales':'sum','Price':'mean'})
+      .reset_index()
+    )
+
+def merge_with_input(agg_df, input_df):
+    return agg_df.merge(input_df, how='left', on=['Market','Channel','SKU','Date'])
 
